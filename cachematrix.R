2ffa5a3834f85cacdf09786fa0a46436712cdfb2
Juan De Leon <antoniom.77@gmail.com>
2015-03-21 14:43:08 -0700

    Modified for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##The first function called makeCacheMatrix, initialize the 
+##functions list  set, get, setinv and getinv.
+##In the "set" function, there is a validation to check if the 
+##matrix that is being set is the sameone as the previous one.
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##The inv cached variable holds the value of the matrix inverse.
+        ##When the fuction is first initialized, the inv is set to NULL.
+        inv <<- NULL
+        ##Every time the matrix is set, I'm checking if the matrix is the first one
+        ##being set, and if not it also checks if the previous one was equal or different.
+        set <- function(y){
+                if (identical(x,y) == FALSE) {
+                        x <<- y
+                        inv <<- NULL
+                }
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+##cacheSolve function, returns the inverse of the matrix
+##from cache is the invers has already been calculated,
+##and calculates it if the inverse has not been calculated
+##or if the one in cache is for a different matrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {                
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
